3b819b6edaa63fbffee5f2e31d4ae5046871aceb
Shubham <subham.k.ojha@gmail.com>
2024-07-13 19:50:30 +0530

    Cleaning Up

--- cmd/core_service/main.go
+++ cmd/core_service/main.go
@@ -18,27 +18,6 @@ func main() {
 	}
 	defer zkClient.Close()
 
-	// // Create brokers
-	// brokers := []string{"broker1", "broker2"}
-	// for _, b := range brokers {
-	// 	err := zkClient.RegisterBroker(b)
-	// 	if err != nil {
-	// 		log.Fatalf("Failed to register broker %s: %v", b, err)
-	// 	}
-	// }
-
-	// //Register a topic and assign it to a broker
-	// topicName := "testTopic"
-	// partitionCount := 3
-	// selectedBroker := brokers[0] // Simplified selection for demonstration
-
-	// err = zkClient.RegisterTopic(topicName, partitionCount, selectedBroker)
-	// if err != nil {
-	// 	log.Fatalf("Failed to register topic %s: %v", topicName, err)
-	// }
-
-	// fmt.Println("Core Service started successfully.")
-
 	// Initialize WAL
 	wal, err := wal.NewWAL(".\\data")
 	if err != nil {
